0d6ff1e685686c5576b11ceabb917cad99723049
Nikhil Patkal <nikhilpatkal9552@gmail.com>
2025-03-23 16:32:49 +0530

    added the folder structure in the project

---
+++ src/app/accout/accout.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AccoutRoutingModule } from './accout-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AccoutRoutingModule
+  ]
+})
+export class AccoutModule { }
